79b0a2de2ad318cb5e2c824160054e2fef3e0091
German Arevalo J <germanarevalojerez@gmail.com>
2022-02-16 10:04:06 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Pokemones from "./components/Pokemones";
+import { Provider } from "react-redux";
+import generateStore from "./redux/store";
+
+function App() {
+  const store = generateStore();
+  return (
+    <Provider store={store}>
+      <div className="container mt-3">
+        <Pokemones />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ src/redux/store.js
@@ -0,0 +1,19 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import pokeReducer from "./pokeDucks";
+
+const rootReducer = combineReducers({
+  pokemones: pokeReducer,
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default function generateStore() {
+  const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk))
+  );
+  return store;
+}
